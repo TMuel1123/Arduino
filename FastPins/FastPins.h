f47d8c57f79f76ffb28e2a40610eaa9f28058963
TMuel1123 <TMuel1123@gmail.com>
2013-12-26 02:03:48 +0100

    Created a lirary/include file to access the pins of the arduino much faster than with the digitalWrite() function. It is also now possible to write more than one pin at a time if bothe are on the same port.

---
+++ FastPins/FastPins.h
@@ -0,0 +1,73 @@
+/*
+ * FastPins.h
+ * Author: Thomas Müller
+ * Copyright 2013 Thomas Müller <tmuel1123 at gmail.com>
+ * 
+ * Some macro definitions for faster pin access of the Arduino
+ * 
+ * The only drawback is that the easy pin assignment of the Arduino can not be
+ * used for this. 
+ */
+
+
+/******************************************************************************
+***   Include                                                               ***
+******************************************************************************/
+
+#if(ARDUINO >= 100)
+ #include <Arduino.h>
+#else
+ #include <WProgram.h>
+#endif
+
+/******************************************************************************
+***   Define                                                                ***
+******************************************************************************/
+
+#define PA PORTA
+#define PB PORTB
+#define PC PORTC
+#define PD PORTD
+#define PE PORTE
+#define PF PORTF
+#define PG PORTG
+#define PH PORTH
+#define PI PORTI
+#define PJ PORTJ
+#define PK PORTK
+#define PL PORTL
+
+#define TRUE  1
+#define FALSE 0
+
+/******************************************************************************
+***   Macro                                                                 ***
+******************************************************************************/
+
+/*
+ * DigitalFastWrite
+ * 
+ * This macro provdies a much faster acces to the arduino output pins.
+ * 
+ * Usage:
+ * digitalFastWrite(Port,Pin number, Output H/L);
+ * digitalFastWrite(PORTB, 5, 1); // Sets the PIN 13 (LED) to a high level
+ * digitalFastWrite(PORTB, 5, 3, 1); // Sets the PIN 13 (LED) and PIN11 to a high level
+ */
+
+#define GET_MACRO(_1,_2,_3,_4,_5,_6,_7,_8,_9,_10,NAME,...) NAME
+#define digitalFastWrite(...) GET_MACRO(__VA_ARGS__, digitalFastWrite8, digitalFastWrite7, digitalFastWrite6, digitalFastWrite5, digitalFastWrite4, digitalFastWrite3, digitalFastWrite2, digitalFastWrite1, digitalFastWrite_Not_enough_arguments)(__VA_ARGS__)
+#define digitalFastWrite1(_port, _pin1, _level)  (_level ? (_port |= (1<<_pin1)) : (_port &= ~(1<<_pin1)))
+#define digitalFastWrite2(_port, _pin1, _pin2, _level)  (_level ? (_port |= ((1<<_pin1)|(1<<_pin2))) : (_port &= ~((1<<_pin1)|(1<<_pin2))))
+#define digitalFastWrite3(_port, _pin1, _pin2, _pin3, _level)  (_level ? (_port |= ((1<<_pin1)|(1<<_pin2)|(1<<_pin3))) : (_port &= ~((1<<_pin1)|(1<<_pin2)|(1<<_pin3))))
+#define digitalFastWrite4(_port, _pin1, _pin2, _pin3, _pin4, _level)  (_level ? (_port |= ((1<<_pin1)|(1<<_pin2)|(1<<_pin3)|(1<<_pin4))) : (_port &= ~((1<<_pin1)|(1<<_pin2)|(1<<_pin3)|(1<<_pin4))))
+#define digitalFastWrite5(_port, _pin1, _pin2, _pin3, _pin4, _pin5, _level)  (_level ? (_port |= ((1<<_pin1)|(1<<_pin2)|(1<<_pin3)|(1<<_pin4)|(1<<_pin5))) : (_port &= ~((1<<_pin1)|(1<<_pin2)|(1<<_pin3)|(1<<_pin4)|(1<<_pin5))))
+#define digitalFastWrite6(_port, _pin1, _pin2, _pin3, _pin4, _pin5, _pin6, _level)  (_level ? (_port |= ((1<<_pin1)|(1<<_pin2)|(1<<_pin3)|(1<<_pin4)|(1<<_pin5)|(1<<_pin6))) : (_port &= ~((1<<_pin1)|(1<<_pin2)|(1<<_pin3)|(1<<_pin4)|(1<<_pin5)|(1<<_pin6))))
+#define digitalFastWrite7(_port, _pin1, _pin2, _pin3, _pin4, _pin5, _pin6, _pin7, _level)  (_level ? (_port |= ((1<<_pin1)|(1<<_pin2)|(1<<_pin3)|(1<<_pin4)|(1<<_pin5)|(1<<_pin6)|(1<<_pin7))) : (_port &= ~((1<<_pin1)|(1<<_pin2)|(1<<_pin3)|(1<<_pin4)|(1<<_pin5)|(1<<_pin6)|(1<<_pin7))))
+#define digitalFastWrite8(_port, _pin1, _pin2, _pin3, _pin4, _pin5, _pin6, _pin7, _pin8, _level)  (_level ? (_port = 0xFF) : (_port = 0x00))
+
+/*
+ * DigitalFastRead
+ */
+
+#define digitalFastRead(_port, _pin) ((_port) & (1<<_pin) ? (1) : (0))
