dde885a66c7b236e283de336757fe60b9dae1226
TMuel1123 <TMuel1123@gmail.com>
2014-01-12 11:58:10 +0100

    Added some comments and fixed some typos

--- FastPins/FastPins.h
+++ FastPins/FastPins.h
@@ -24,6 +24,7 @@
 ***   Define                                                                ***
 ******************************************************************************/
 
+// Ports
 #define PA PORTA
 #define PB PORTB
 #define PC PORTC
@@ -50,16 +51,26 @@
  * This macro provdies a much faster acces to the arduino output pins.
  * 
  * The price for the faster pin access are the following drawbacks:
- *  * The easy pin assignment fron the arduino is not possible. Now PORT and PIN must be known
+ *  * The easy pin assignment from the Arduino is not possible. Now PORT and PIN must be known
  *  * Different Arduinos (UNO, MEGA, LEONARDO...) have different pin assignments: PIN13 (LED) is PORTB Pin5 on the UNO, PORTB Pin7 on the MEGA and PORTC Pin7 on the Leonardo
  *  * Usage makes the Arduino sketch fixed to one board due to different pin assignment
  *  * If a pin is configured as input the internal pullup resitor will be activated
  * 
  * Usage:
  * digitalFastWrite(Port,Pin number, Output H/L);
- * digitalFastWrite(PC, 150);  // Sets the complete PORTC to 214 which is 0b10010110
+ * digitalFastWrite(PC, 150);  // Sets the complete PORTC to 150 which is 0b10010110
  * digitalFastWrite(PB, 5, 1); // Sets the pin number 5 of PORTB true which is PIN13 (LED) on an Arduino UNO
  * digitalFastWrite(PB, 5, 3, 1); // Sets the PIN13 (LED) and PIN11 to a high level
+ * 
+ * To set an pin as an output the corrosponding bit in the DDRx (Data Direction Register) must be set to 1.
+ * The initial state after power on is that all pins are configured as inputs
+ * 
+ * Example:
+ * digitalFastWrite(DDRC, 0,1,2, OUTPUT); // sets pins 0,1 and 2 as outputs
+ * digitalFastWrite(DDRC, 0,1,2, INPUT);  // sets pins 0,1 and 2 as inputs
+ * 
+ * The constant INPUT_PULLUP does not work with this macros!
+ * 
  */
 
 #define GET_MACRO(_1,_2,_3,_4,_5,_6,_7,_8,_9,_10,NAME,...) NAME
@@ -74,8 +85,15 @@
 #define digitalFastWrite7(_port, _pin1, _pin2, _pin3, _pin4, _pin5, _pin6, _pin7, _level)  (_level ? (_port |= ((1<<_pin1)|(1<<_pin2)|(1<<_pin3)|(1<<_pin4)|(1<<_pin5)|(1<<_pin6)|(1<<_pin7))) : (_port &= ~((1<<_pin1)|(1<<_pin2)|(1<<_pin3)|(1<<_pin4)|(1<<_pin5)|(1<<_pin6)|(1<<_pin7))))
 #define digitalFastWrite8(_port, _pin1, _pin2, _pin3, _pin4, _pin5, _pin6, _pin7, _pin8, _level)  (_level ? (_port = 0xFF) : (_port = 0x00))
 
+
 /*
  * DigitalFastRead
+ * 
+ * The input and output registers are at different addresses. For this reason use PINx instead of PORTx or Px.
+ * If readed from the address of PORTx the status of the internal pullup resistors is read which could be different form the real input state
+ * 
+ * Usage:
+ * byte myVariable = digitalFastRead(PINC, 2); // Reads Pin 2 from PORTC and writes the result into the mariable myVariable
  */
 
 #define digitalFastRead(_port, _pin) ((_port) & (1<<_pin) ? (1) : (0))
